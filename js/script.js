5c310968d27c72d52a450e6e82dcee0f2e2662f1
rosiemk <rosemarymunrokerr@rosemarys-air.home>
2015-12-02 19:51:45 +0000

    button updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function{
+	$("button").mouseenter(function(){
+		$("button").fadeTo("slow",0.5);
+	});
+	$("button").mouseleave(function(){
+		$("button").fadeTo("fast",1);
+	});
+});
