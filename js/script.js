c4f1223818c7011de2b9a43f737badbd17166a01
rosiemk <rosemarymunrokerr@rosemarys-air.arts.local>
2015-12-03 15:01:25 +0000

    top section of story is scrolling with head images.Addition of stories into script

--- js/script.js
+++ js/script.js
@@ -17,6 +17,18 @@ $(document).ready(function(){
 	$("#bottom_1").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
 	$("#bottom_2").css('display','none');
 	$("#bottom_3").css('display','none');
+	$("#story_head_1").show();
+	$("#story_head_2").hide();
+	$("#story_head_3").hide();
+	$("#story_head_4").hide();
+	$("#story_middle_1").show();
+	$("#story_middle_2").hide();
+	$("#story_middle_3").hide();
+	$("#story_middle_4").hide();
+	$("#story_bottom_1").show();
+	$("#story_bottom_2").hide();
+	$("#story_bottom_3").hide();
+	$("#story_bottom_4").hide();
 
 });
 
@@ -29,12 +41,82 @@ function headprev(){
 	else 
 		{  current_head_index = current_head_index - 1}
 
+
+	if (current_head_index == 1)
+ {
+ 	$("#head_1").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
+	$("#head_2").css('display','none');
+	$("#head_3").css('display','none');
+	$("#head_4").css('display','none');
+	$("#story_head_1").show();
+	$("#story_head_2").hide();
+	$("#story_head_3").hide();
+	$("#story_head_4").hide();
+ 	return;
+ }
+
+  if (current_head_index == 2)
+ {
+ 	$("#head_1").css('display','none');
+	$("#head_2").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
+	$("#head_3").css('display','none');
+	$("#head_4").css('display','none');
+	$("#story_head_1").hide();
+	$("#story_head_2").show();
+	$("#story_head_3").hide();
+	$("#story_head_4").hide();
+ 	return;
+ }
+
+   if (current_head_index == 3)
+ {
+ 	$("#head_1").css('display','none');
+	$("#head_2").css('display','none');
+	$("#head_3").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
+	$("#head_4").css('display','none');
+	$("#story_head_1").hide();
+	$("#story_head_2").hide();
+	$("#story_head_3").show();
+	$("#story_head_4").hide();
+ 	return;
+ }
+
+   if (current_head_index == 4)
+ {
+ 	$("#head_1").css('display','none');
+	$("#head_2").css('display','none');
+	$("#head_3").css('display','none');
+	$("#head_4").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
+	$("#story_head_1").hide();
+	$("#story_head_2").hide();
+	$("#story_head_3").hide();
+	$("#story_head_4").show();
+ 	return;
+ }
+}
+
+function headnext(){
+
+ 	if (current_head_index == 4)
+	 {current_head_index = 1 ;}
+ 	else 
+ 		{  current_head_index = current_head_index + 1}
+
+
+
+
+//delete this if it doesnt work
+
 if (current_head_index == 1)
  {
  	$("#head_1").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
 	$("#head_2").css('display','none');
 	$("#head_3").css('display','none');
 	$("#head_4").css('display','none');
+	$("#story_head_1").show();
+	$("#story_head_2").hide();
+	$("#story_head_3").hide();
+	$("#story_head_4").hide();
 
  	return;
  }
@@ -45,6 +127,10 @@ if (current_head_index == 1)
 	$("#head_2").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
 	$("#head_3").css('display','none');
 	$("#head_4").css('display','none');
+	$("#story_head_1").hide();
+	$("#story_head_2").show();
+	$("#story_head_3").hide();
+	$("#story_head_4").hide();
  	return;
  }
 
@@ -54,6 +140,10 @@ if (current_head_index == 1)
 	$("#head_2").css('display','none');
 	$("#head_3").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
 	$("#head_4").css('display','none');
+	$("#story_head_1").hide();
+	$("#story_head_2").hide();
+	$("#story_head_3").show();
+	$("#story_head_4").hide();
  	return;
  }
 
@@ -63,6 +153,10 @@ if (current_head_index == 1)
 	$("#head_2").css('display','none');
 	$("#head_3").css('display','none');
 	$("#head_4").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
+	$("#story_head_1").hide();
+	$("#story_head_2").hide();
+	$("#story_head_3").hide();
+	$("#story_head_4").show();
  	return;
  }
 
@@ -70,56 +164,4 @@ if (current_head_index == 1)
 }
 
 
-function headnext(){
-
-	if (current_head_index == 4)
-	 {current_head_index = 1 ;}
-	else 
-		{  current_head_index = current_head_index + 1}
-
-
- if (current_head_index == 1)
- {
- 	$("#head_1").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
-	$("#head_2").css('display','none');
-	$("#head_3").css('display','none');
-	$("#head_4").css('display','none');
-
- 	return;
- }
-
-  if (current_head_index == 2)
- {
- 	$("#head_1").css('display','none');
-	$("#head_2").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
-	$("#head_3").css('display','none');
-	$("#head_4").css('display','none');
-
- 	return;
- }
-
-   if (current_head_index == 3)
- {
- 	$("#head_1").css('display','none');
-	$("#head_2").css('display','none');
-	$("#head_3").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
-	$("#head_4").css('display','none');
-
- 	return;
- }
-
-   if (current_head_index == 4)
- {
- 	$("#head_1").css('display','none');
-	$("#head_2").css('display','none');
-	$("#head_3").css('display','none');
-	$("#head_4").css({'display':'block', 'margin-left':'auto' , 'margin-right':'auto' });
-
- 	return;
- }
-
-
-
-
-}
 
